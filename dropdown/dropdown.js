33709a0e3209499c8fe1de0e13ca83ef085aa572
Harry <64039651+hswinata@users.noreply.github.com>
2023-09-21 14:13:59 +0200

    Move files around

--- dropdown/dropdown.js
+++
@@ -1,8 +0,0 @@
-const dropDown = () => {
-    let menu = document.querySelector('.menu');
-    menu.addEventListener('click', e => {
-        document.querySelector('.dropdown').classList.toggle('visible');
-    });
-};
-
-export { dropDown };
